f42220b2d4b1e43729445b6d8b652d5e28bec874
jackyflo22 <156362162+jackyflo22@users.noreply.github.com>
2024-01-16 15:52:33 -0800

    Update test_demo.py

--- test_demo.py
+++ test_demo.py
@@ -1,11 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-
-service = Service(executable_path=ChromeDriverManager().install())
+from Driver import service
 
 driver = webdriver.Chrome(service=service)
+
 
 class TestItemClass:
 
