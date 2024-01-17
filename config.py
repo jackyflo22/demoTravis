90ad1fc5a0763e7af2d3b73f06a03b375c869278
jackyflo22 <156362162+jackyflo22@users.noreply.github.com>
2024-01-16 19:39:08 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 TEST_ENVIRONMENT = os.getenv('TEST_ENVIRONMENT', None)
-RUN_REMOTE = os.getenv("RUN_REMOTE")
+RUN_REMOTE = True
 BROWSER_TYPE = os.getenv("BROWSER", 'chrome')  # supported 'chrome', and 'firefox'
 SELENIUM_GRID_HUB = os.getenv("SELENIUM_GRID_HUB")
 OPEN_BROWSER_WINDOW_ON_MAIN_DISPLAY = os.getenv("OPEN_BROWSER_WINDOW_ON_MAIN_DISPLAY", 'False')
